dc6f91ed5e7c54489aff6c67a0c111adf0ac0be7
kpotoh <axepon@mail.ru>
2022-09-09 16:13:28 +0200

    some EDA before glm

--- scripts/download_human_mt/get_genbank_files.sh
+++ scripts/download_human_mt/get_genbank_files.sh
@@ -2,10 +2,10 @@
 
 ACC_LIST=data/1raw/human_mt_acc.txt
 GB_FILE=data/1raw/human_mt_seqs.gb
-LAG=1
+LAG=0.3
 
 touch $GB_FILE
-for acc in `cat $ACC_LIST`; do
+for acc in `tail -n 38500 $ACC_LIST`; do
     cnt=`grep -c $acc $GB_FILE`
     if [ $cnt == 0 ]; then
         echo "Downloading $acc"
